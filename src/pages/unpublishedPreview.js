db78f73fd44f82d0018cc76aa3b8b2ff2b9970f1
Sam Logan <sam@sjlwebdesign.com>
2019-08-03 10:30:21 +1000

    Fix window object breaking build

--- src/pages/unpublishedPreview.js
+++ src/pages/unpublishedPreview.js
@@ -3,6 +3,8 @@ import { navigate } from 'gatsby';
 import { Page } from '../templates/page';
 
 export const UnpublishedPage = props => {
+  const IS_BROWSER = typeof window !== 'undefined';
+  if (!IS_BROWSER) return null;
   const previewData = window.__PRISMIC_PREVIEW_DATA__;
   if (!previewData) {
     navigate('/404');
