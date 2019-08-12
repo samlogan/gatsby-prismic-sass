f91139002a833e85c255c347ac3b39875a36780b
samlogan <sam@sjlwebdesign.com>
2019-08-12 17:59:27 +1000

    Included other components in export file

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,11 @@
 import SEO from './SEO';
 import { Footer } from './Footer';
 import { Header } from './Header';
+import { Image } from './Image';
 import { Layout } from './Layout';
+import { Logo } from './Logo';
+import Modal from './Modal';
+import { Navigation } from './Navigation';
+import Video from './Video';
 
-export { Footer, Layout, SEO, Header };
+export { SEO, Footer, Header, Image, Layout, Logo, Modal, Navigation, Video };
