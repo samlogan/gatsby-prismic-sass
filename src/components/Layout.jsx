cbfd88ee2542c6b47afcc7cf75903a6051c77241
samlogan <sam@sjlwebdesign.com>
2019-08-22 06:57:21 +1000

    Removed unused static query from Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
 import { Header } from './Header';
 import { Footer } from './Footer';
 import SEO from './SEO';
@@ -8,31 +7,16 @@ import 'typeface-lato';
 import '../sass/global/styles.scss';
 import './Layout.scss';
 
-const query = graphql`
-  query LayoutQuery {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
-
 export const Layout = props => {
   const { children, customSEO } = props;
   return (
-    <StaticQuery
-      query={query}
-      render={data => (
-        <>
-          <Header />
-          <main>
-            {!customSEO && <SEO />}
-            {children}
-          </main>
-          <Footer />
-        </>
-      )}
-    />
+    <>
+      <Header />
+      <main>
+        {!customSEO && <SEO />}
+        {children}
+      </main>
+      <Footer />
+    </>
   );
-}
+};
