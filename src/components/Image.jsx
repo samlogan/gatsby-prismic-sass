81e57951427faba627e9e9a0fe01349f6d0af7aa
samlogan <sam@sjlwebdesign.com>
2019-08-22 07:05:50 +1000

    Added alt to Gatsby Image component

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -6,7 +6,13 @@ export const Image = props => {
   const { image, className = '' } = props;
   if (!image) return <div className={`gatsby-image placeholder ${className}`} />;
   if (image.localFile && image.localFile.childImageSharp && image.localFile.childImageSharp.fluid) {
-    return <Img className={`gatsby-image ${className}`} fluid={image.localFile.childImageSharp.fluid} />;
+    return (
+      <Img
+        className={`gatsby-image ${className}`}
+        fluid={image.localFile.childImageSharp.fluid}
+        alt={image.alt || ''}
+      />
+    );
   }
   if (image.url) {
     return <img className={`gatsby-image ${className}`} src={image.url} alt={image.alt || ''} />;
