8a875ce4a976bdd5da879620cbb1be014da345cc
samlogan <sam@sjlwebdesign.com>
2019-08-22 06:56:19 +1000

    Fetch siteName in Logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import { siteName } from '../../config/website';
 import './Logo.scss';
 
 export const Logo = () => (
   <Link className="logo" to="#">
-    Logo
+    {siteName}
   </Link>
 );
