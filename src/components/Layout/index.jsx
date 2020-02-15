2f677ffb768dbcccb791772975e344838fec70c0
samlogan <sam@sjlwebdesign.com>
2020-02-16 06:38:38 +1100

    Removed secondary font example

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Footer, Header, SEO } from 'components';
-import 'typeface-titillium-web';
 import 'typeface-lato';
 import '../../sass/global/styles.scss';
 import './styles.scss';
