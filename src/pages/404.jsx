db49b135922644eb41e426df928ce6e4b23fe9dc
samlogan <sam@sjlwebdesign.com>
2020-02-16 07:49:51 +1100

    Made 404 page a functional component

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,26 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 import { Layout } from 'components';
 import FourOhFourImage from 'images/404.svg';
 import './404.scss';
 
-class FourOhFour extends Component {
-  render() {
-    const { location } = this.props;
-    return (
-      <Layout location={location}>
-        <div className="wrapper not-found-wrap">
-          <img className="not-found-image" src={FourOhFourImage} alt="Opps this page has gone missing" />
-          <div className="not-found-text">
-            <h2>404 Not found</h2>
-            <Link className="button" to="/">
-              Go back home
-            </Link>
-          </div>
-        </div>
-      </Layout>
-    );
-  }
-}
+const FourOhFour = ({ location }) => (
+  <Layout location={location}>
+    <div className="wrapper not-found-wrap">
+      <img className="not-found-image" src={FourOhFourImage} alt="Opps this page has gone missing" />
+      <div className="not-found-text">
+        <h2>404 Not found</h2>
+        <Link className="button" to="/">
+          Go back home
+        </Link>
+      </div>
+    </div>
+  </Layout>
+);
 
 export default FourOhFour;
