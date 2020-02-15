964dc023ab93f511a41f5c97e17ec2f9d076bfac
samlogan <sam@sjlwebdesign.com>
2020-02-16 06:35:15 +1100

    Improved slices structure and export using require context

--- src/slices/index.js
+++ src/slices/index.js
@@ -1,3 +1,7 @@
-import { IntroSection } from './IntroSection/IntroSection';
+const context = require.context('./', true, /\.\/[^/]+\/index\.jsx$/);
 
-export { IntroSection };
+context.keys().forEach(filePath => {
+  // Remove the './' and './svg' from the object key
+  const componentName = filePath.replace(/^.+\/([^/]+)\/index\.jsx/, '$1');
+  module.exports[componentName] = context(filePath).default;
+});
