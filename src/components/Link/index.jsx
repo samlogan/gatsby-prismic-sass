ce2aadc73375378f939f4539384e463963507ce9
samlogan <sam@sjlwebdesign.com>
2020-02-16 07:47:43 +1100

    Fixed Link export and added onClick prop

--- src/components/Link/index.jsx
+++ src/components/Link/index.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Link as GatsbyLink } from 'gatsby';
 
-export const Link = props => {
-  const { children, className, to, title, target = '_blank' } = props;
+const Link = props => {
+  const { children, className, to, title, target = '_blank', onClick = () => {} } = props;
   const isExternal = (to && to.indexOf('http') !== -1) || (to && to[0] === '#');
   if (isExternal) {
     return (
-      <a href={to} className={className || ''} title={title || null} target={target} rel="nofollow noopener noreferrer">
+      <a
+        href={to}
+        className={className || ''}
+        title={title || null}
+        target={target}
+        onClick={onClick}
+        rel="nofollow noopener noreferrer"
+      >
         {children}
       </a>
     );
   }
   return (
-    <GatsbyLink to={to} className={className || ''} title={title || null}>
+    <GatsbyLink to={to} className={className || ''} title={title || null} onClick={onClick}>
       {children}
     </GatsbyLink>
   );
 };
+
+export default Link;
